43384fc8c2e42e4f5e80d6bc14ffbc370b876eff
LokeshBanarse <lokesh.banarse2000@gmail.com>
2023-05-10 23:57:01 +0530

    create hello.js

---
+++ hello.js
@@ -0,0 +1,16 @@
+console.log("Lokesh Banarse..");
+
+var fristName = "Lokesh"; // first name
+console.log(fristName);
+
+var age; // Variable Declaration
+age = "22"; // Variable Initialization
+console.log(age);
+
+var holder; // Variable Declaration
+console.log(holder); // undefined
+holder = "My Spec"; // Variable Initialization
+console.log(holder);
+
+holder = "Sachin Spec"; // Variable update
+console.log(holder); //
