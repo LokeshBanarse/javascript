11994c45a3818d3e755e2c816d5995b17a647d16
LokeshBanarse <lokesh.banarse2000@gmail.com>
2023-05-11 21:14:53 +0530

    01_variable_assign.js
    Assignment

---
+++ 01_variable_assign.js
@@ -0,0 +1,23 @@
+var myFullName  //Declare a variable with name → myFullName
+console.log(myFullName);   
+
+myFullName="Lokesh Banarse"// Initialize the myFullName with your first and last name only.
+console.log(myFullName);
+
+myFullName = "Lokesh Pundalikrao Banarse";// Update the myFullName variable and with your first name, middle name and last name.
+console.log(myFullName); 
+
+var myLove;             // Variable Declaration
+myLove="I love only JS"// Variable Initialization
+console.log(myLove)
+
+var fristName ="Lokesh"
+var lastName = "Banarse";
+var age = 22;
+var address = "Amravati, Mharashtra";
+var pincode = "444709";
+var gender = "Male";
+var isMarried = false;
+
+console.log(fristName,lastName,age,address,pincode,gender,isMarried); 
+
