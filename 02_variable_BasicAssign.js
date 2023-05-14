d42982c603c740400904260250ef4dd3d14969fa
LokeshBanarse <lokesh.banarse2000@gmail.com>
2023-05-14 22:51:10 +0530

    add 02_variable_BasicAssign.js

---
+++ 02_variable_BasicAssign.js
@@ -0,0 +1,16 @@
+// Variable declarations
+var numberVariable = 123;
+var stringVariable = "Hello";
+var booleanVariable = true;
+
+// Log variable values and types
+console.log("numberVariable:", numberVariable, "type:", typeof numberVariable);
+console.log("stringVariable:", stringVariable, "type:", typeof stringVariable);
+console.log("booleanVariable:", booleanVariable, "type:", typeof booleanVariable);
+
+// Difference between null and undefined
+var nullVariable = null;
+var undefinedVariable;
+
+console.log("nullVariable:", nullVariable, "type:", typeof nullVariable);
+console.log("undefinedVariable:", undefinedVariable, "type:", typeof undefinedVariable);
