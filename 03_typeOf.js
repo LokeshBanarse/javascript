431a608ba8083c5e093aada1677970a857643313
LokeshBanarse <lokesh.banarse2000@gmail.com>
2023-05-16 13:44:50 +0530

    Update 03_typeOf.js

--- 03_typeOf.js
+++ 03_typeOf.js
@@ -1,29 +1,25 @@
-// Declare variable bankName and log its value and data type before initialization
+
 var bankName;
 console.log("bankName:", bankName, "type:", typeof bankName);
 
-// Initialize variable bankName and log its value and data type
+
 bankName = "SBI Bank";
 console.log("bankName:", bankName, "type:", typeof bankName);
 
-// Declare and initialize variable marks and log its value and data type
 var marks = '90%';
 console.log("marks:", marks, "type:", typeof marks);
 
-// Declare variable isWorking and log its value before initialization
+
 var isWorking;
 console.log("isWorking:", isWorking, "type:", typeof isWorking);
 
-// Initialize variable isWorking and log its value after initialization
 isWorking = true;
 console.log("isWorking:", isWorking, "type:", typeof isWorking);
 
-// Declare and initialize variable totalCount with a string value
 var totalCount = "Hundred and Seven";
 
-// Log the value and data type of totalCount as it is
 console.log("totalCount:", totalCount, "type:", typeof totalCount);
 
-// Update variable totalCount with a new value and log it along with its data type
+
 totalCount = 107;
 console.log("totalCount:", totalCount, "type:", typeof totalCount);
