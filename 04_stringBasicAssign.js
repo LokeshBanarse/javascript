9c6111411f75c7f58c316aad2b66ae99479e9593
LokeshBanarse <>
2023-05-22 22:35:34 +0530

    Create 04_stringBasicAssign.js

---
+++ 04_stringBasicAssign.js
@@ -0,0 +1,11 @@
+function stringBasics() {
+  var dreamCopany = "My dream company is Google";
+  console.log(dreamCopany);
+
+  var hobby1 = "Coding";
+  var hobby2 = "Reading";
+  var hobby3 = "Playing Cricket";
+
+  console.log("My hobbies are: " + hobby1 + ", " + hobby2 + ", " + hobby3);
+}
+stringBasics();
