e8fae88b988aea9f75db776baf31c1a896e8616d
LokeshBanarse <130983255+LokeshBanarse@users.noreply.github.com>
2023-06-01 15:22:21 +0530

    Update 05_operatorA.js

--- 05_operatorA.js
+++ 05_operatorA.js
@@ -12,4 +12,32 @@ squareOfWordLength( "Developer Smart");
 
 console.log("---------------------------------------------------------");
 
+function  stringLength() {
+  var mystring = "I am Angular Developer";
+  var mystringLength = mystring.length;
+  console.log(`${mystring}`);
+  console.log(`Length is ${mystringLength}`);
+
+  var stringWords = mystring.split(" ")
+  console.log(stringWords );
+}
+stringLength();
+
+
+console.log("---------------------------------------------------------");
+function  stringass( ){
+    var stringName = "I am Angular Developer";
+    var stringNameLength = stringName.length;
+    console.log(`Given string length is ${ stringNameLength }` );
+
+    var stringWords = stringName.split(" ")
+    console.log(stringWords );
+    var p=stringWords.length
+     console.log(p );
+   var div = stringNameLength / p;
+   console.log(`String Length And divid by total number words available in string is ${div}`);
+    var multiplication = stringNameLength * p;
+   console.log(`String Length And Multiply by total number words available in string is ${multiplication }`);
+}
+stringass ( );
 
