2101f5d370f2544f7bd7a436e22dd64e9721dab0
LokeshBanarse <130983255+LokeshBanarse@users.noreply.github.com>
2023-06-01 15:22:05 +0530

    Create 04_stringHandsOn.js

---
+++ 04_stringHandsOn.js
@@ -0,0 +1,30 @@
+ function stringHandsOn()
+ {
+    var string = "   Hey you are doing good, keep it up    ";
+    console.log(string)
+
+    var length = string.length
+    console.log("Length is :",length);
+
+    console.log("--------------------------------------------");
+    
+    console.log(string) 
+    var length = string.length
+    console.log("Length is :",length); 
+
+    console.log("--------------------------------------------");
+
+    var trim = string.trim()
+    console.log(trim);
+    console.log("Remove Extra  Space & Length is :",trim.length);
+
+    console.log("--------------------------------------------");
+
+    var line = "hey | you| are | doing | good | keep | it | up";
+    var assign = line.split("|");
+    console.log(assign);
+    
+  }
+  stringHandsOn();
+  
+  
